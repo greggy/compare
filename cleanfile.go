39351619cb0048c852c3cd16d09679c4caca9414
Grigory Fateyev <gfborn@gmail.com>
2017-02-10 15:24:40 +0300

    Fix clean file util.

--- cleanfile.go
+++ cleanfile.go
@@ -5,35 +5,39 @@ import (
   "fmt"
   "log"
   "os"
-  "strings"	
-  "io/ioutil"
-  "regexp"
+  "strings"
 )
 
 func main() {
-
   args := os.Args[1:]
   if len(args) != 1 {
     fmt.Println("You should specify one argument.")
   } else {
-
     f_path1 := args[0]
 
-    file, err := ioutil.ReadFile(f_path1)
+    file, err := os.Open(f_path1)
     if err != nil {
       log.Fatalf("readLines in file 1: %s", err)
     }
 
-    data := string(file)
-    cdata := strings.Replace(data, "\r", "\n", -1)
-    cdata2 := strings.Replace(cdata, "\n\n", "\n", -1)
-    re := regexp.MustCompile(`(?m)^\s*$[\r\n]*|[\r\n]+\s+\z`)
-    cdata3 := re.ReplaceAllString(cdata2, "")
-    cdata4 := strings.Replace(cdata3, "\t", "", -1)
-
     rfile, err := os.Create("c" + f_path1)
+    if err != nil {
+      log.Fatalf("writeLines: %s", err)
+    }
+    defer rfile.Close()
+
     w := bufio.NewWriter(rfile)
-    _, err = w.WriteString(cdata4)
+
+    scanner := bufio.NewScanner(file)
+    scanner.Split(bufio.ScanWords)
+    for scanner.Scan() {
+      line := scanner.Text()
+      cdata := strings.TrimSpace(line)
+      cdata2 := strings.Trim(cdata, "\n")	    
+      if cdata2 != "" && cdata2 != "\n" && cdata2 != "\r\n" && cdata2 != "\t" {
+        fmt.Fprintln(w, cdata2)
+      }
+    }
 
     fmt.Printf("Cleaned data was written to %s file.\n", "c" + f_path1)
     w.Flush()
